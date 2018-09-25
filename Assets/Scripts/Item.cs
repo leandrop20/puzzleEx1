168027c9fa7bdba7661e305bd9cb0bdac0fcee9b
LeandroP <byleandropinheiro@gmail.com>
2018-09-25 16:53:10 -0300

    created move

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,58 +5,84 @@ using DG.Tweening;
 
 public class Item : MonoBehaviour {
 
-    [HideInInspector]
-    public string id;
-
     private Vector2 initPosition;
     private float DRAG_LIMIT = 0.7f;
+    private float MOVE_LIMIT = 0.1f;
     private bool isDragging = false;
-
-    void Awake() {
-        id = name;
-    }
+    private bool inAnimate = false;
 
     void OnMouseDown() {
-        initPosition = transform.position;
-        isDragging = true;
+        if (!isDragging && !inAnimate) {
+            initPosition = transform.position;
+            isDragging = true;
+        }
     }
 
     void OnMouseUp() {
-        isDragging = false;
-        Back();
-    }
-
-    void OnCollisionEnter2D(Collision2D collision) {
-        Debug.Log("COLISSION: " + collision.gameObject.name);
-    }
-
-    private void OnTriggerExit2D(Collider2D collision) {
-        Debug.Log("TRIGGER: " + collision.gameObject.name);
+        if (isDragging) {
+            CallChangeObject(GetDirection());
+        }
     }
 
     void Update() {
-        if (isDragging) {
+        if (isDragging && !inAnimate) {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(mousePosition.x, mousePosition.y, -1f);
 
-            if ((initPosition.x + DRAG_LIMIT) < transform.position.x) {
-                Debug.Log("DIREITA");
-                Back();
-            } else if ((initPosition.x - DRAG_LIMIT) > transform.position.x) {
-                Debug.Log("ESQUERDA");
-                Back();
-            } else if ((initPosition.y + DRAG_LIMIT) < transform.position.y) {
-                Debug.Log("CIMA");
-                Back();
-            } else if ((initPosition.y - DRAG_LIMIT) > transform.position.y) {
-                Debug.Log("BAIXO");
-                Back();
+            if (CanMove(mousePosition)) {
+                transform.position = new Vector3(mousePosition.x, mousePosition.y, -1f);
             }
         }
     }
 
+    public void CallChangeObject(Puzzle.DIRECTION direction) {
+        isDragging = false;
+        if (!GetComponentInParent<Puzzle>().ChangeObject(gameObject, initPosition, direction)) {
+            Back();
+        }
+    }
+
+    float GetAngle(Vector2 a, Vector2 b) {
+        Vector2 direction = (Vector2)b - a;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if (angle < 0) angle += 360;
+        return angle;
+    }
+
+    Puzzle.DIRECTION GetDirection() {
+        float angle = GetAngle(initPosition, transform.position);
+        if (angle >= 0 && angle <= 45  || angle <= 360 && angle >= 315) {
+            return Puzzle.DIRECTION.RIGHT;
+        } else if (angle >= 135 && angle <= 225) {
+            return Puzzle.DIRECTION.LEFT;
+        } else if (angle < 135 && angle > 45) {
+            return Puzzle.DIRECTION.UP;
+        } else if  (angle > 225 && angle < 315) {
+            return Puzzle.DIRECTION.BOTTOM;
+        }
+        return Puzzle.DIRECTION.NULL;
+    }
+
+    bool CanMove(Vector2 mousePosition) {
+        if ((initPosition.x + DRAG_LIMIT) > mousePosition.x &&
+            (initPosition.x - DRAG_LIMIT) < mousePosition.x &&
+            (initPosition.y + DRAG_LIMIT) > mousePosition.y &&
+            (initPosition.y - DRAG_LIMIT) < mousePosition.y) {
+            return true;
+        }
+        return false;
+    }
+
+    public void Move(Vector2 p) {
+        inAnimate = true;
+        transform.DOMove(p, 0.3f).OnComplete(() => {
+            inAnimate = false;
+            initPosition = transform.position;
+        });
+    }
+
     void Back() {
-        transform.DOMove(initPosition, 0.3f);
+        inAnimate = true;
+        transform.DOMove(initPosition, 0.3f).OnComplete(() => { inAnimate = false; });
     }
 
 }
