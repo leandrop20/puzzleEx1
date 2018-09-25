c2d474f928d4174364a13816e954e946f682463c
LeandroP <byleandropinheiro@gmail.com>
2018-09-24 21:52:40 -0300

    start project

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Item : MonoBehaviour {
+
+    [HideInInspector]
+    public string id;
+
+    private Vector2 initPosition;
+    private float DRAG_LIMIT = 0.7f;
+    private bool isDragging = false;
+
+    void Awake() {
+        id = name;
+    }
+
+    void OnMouseDown() {
+        initPosition = transform.position;
+        isDragging = true;
+    }
+
+    void OnMouseUp() {
+        isDragging = false;
+        Back();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("COLISSION: " + collision.gameObject.name);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        Debug.Log("TRIGGER: " + collision.gameObject.name);
+    }
+
+    void Update() {
+        if (isDragging) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mousePosition.x, mousePosition.y, -1f);
+
+            if ((initPosition.x + DRAG_LIMIT) < transform.position.x) {
+                Debug.Log("DIREITA");
+                Back();
+            } else if ((initPosition.x - DRAG_LIMIT) > transform.position.x) {
+                Debug.Log("ESQUERDA");
+                Back();
+            } else if ((initPosition.y + DRAG_LIMIT) < transform.position.y) {
+                Debug.Log("CIMA");
+                Back();
+            } else if ((initPosition.y - DRAG_LIMIT) > transform.position.y) {
+                Debug.Log("BAIXO");
+                Back();
+            }
+        }
+    }
+
+    void Back() {
+        transform.DOMove(initPosition, 0.3f);
+    }
+
+}
---
+++ Assets/Scripts/Puzzle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour {
+
+    public GameObject[] items;
+
+    private Vector3 INIT_POSITION = new Vector2(-2.45f, 2.45f);
+    private int ROWS = 8;
+    private int COLS = 8;
+
+    public void Create() {
+        int id;
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                id = Random.Range(0, items.Length);
+                GameObject item = Instantiate(items[id], 
+                    new Vector3(0.7f * i, -0.7f * j, 0) + INIT_POSITION, Quaternion.identity, transform);
+                item.name = i + "_" + j;
+                Debug.Log(item.GetComponent<Item>().id);
+            }
+        }
+    }
+
+    public void Move() {
+        Debug.Log("MOVE");
+    }
+
+}
