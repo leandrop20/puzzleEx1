da6de5721fc2ffdca4ef9543181418797c43a5c6
LeandroP <byleandropinheiro@gmail.com>
2018-10-02 00:18:37 -0300

    inserted score bar

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    private Board board;
+    public Text scoreText;
+    public int score;
+    public Image scoreBar;
+
+    private void Start() {
+        board = FindObjectOfType<Board>();
+    }
+
+    private void Update() {
+        scoreText.text = "" + score;
+    }
+
+    public void IncreaseScore(int amountToIncrease) {
+        score += amountToIncrease;
+        if (board != null && scoreBar != null) {
+            int length = board.scoreGoals.Length;
+            scoreBar.fillAmount = (float)score / (float)board.scoreGoals[length - 1];
+        }
+    }
+
+}
